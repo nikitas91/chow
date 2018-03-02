1b454880742677f4edd5b600dcf7738da71e4424
Nikita Sasunkevich <nikita@nikitas-mbp.lan>
2018-03-02 07:37:55 -0700

    updated server.js to use passport and cookie session

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
+const db = require("./models");
+const passport = require("passport");
+const passportSetup = require("./config/auth");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 3000;
 
+var app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const hbs = exphbs.create({
-    defaultLayout: "main",
-    layoutDir: __dirname + "/views/layouts",
-    partialsDir: [
-        __dirname + "/views"
-    ]
-});
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.engine("handlebars", hbs.engine);
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
+require("./routes/auth-routes.js")(app);
 
-app.listen(PORT, function(){
-	console.log("server started on port " + PORT);
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("Server listening at localhost:" + PORT);
+    });
 });
