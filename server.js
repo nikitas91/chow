09b2ebb163dc27b2f2d540fd775b7dbc1cb5ff57
Nikita Sasunkevich <nikita@nikitas-mbp.lan>
2018-03-03 06:24:08 -0700

    finished server.js

--- server.js
+++ server.js
@@ -24,11 +24,10 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+require("./routes/routes.js")(app);
 require("./routes/auth-routes.js")(app);
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log("Server listening at localhost:" + PORT);
     });
